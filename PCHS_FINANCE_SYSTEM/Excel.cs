12b16ba641cbfb532e103e10315c99fcd4390bc3
m0L3cuL3 <skidrowloler@gmail.com>
2021-02-12 10:14:46 +0800

    commit.

---
+++ PCHS_FINANCE_SYSTEM/Excel.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronXL;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace PCHS_FINANCE_SYSTEM
+{
+    
+
+    enum Storage
+    {
+        //DRIVE_C, --> Will cause UnauthorizedAccessException if used.
+        DRIVE_D,
+        DRIVE_E,
+        DRIVE_F,
+        DRIVE_G,
+        DRIVE_O,
+        DRIVE_P,
+    }
+
+    interface IExcel
+    {
+        void CreateNewWorkbook(string filename);
+    }
+
+    class Excel : IExcel
+    {
+
+        public void CreateNewWorkbook(string filename)
+        {
+            WorkBook wb = WorkBook.Create(ExcelFileFormat.XLSX);
+            wb.Metadata.Author = "PCHS";
+
+            #region Workbook Sheets
+            WorkSheet wsGeneralLedger = wb.CreateWorkSheet("General Ledger");
+            #endregion
+
+
+            #region Workbook Header
+            wsGeneralLedger.Merge("A1:F1");
+            wsGeneralLedger["A1:F1"].Style.HorizontalAlignment = IronXL.Styles.HorizontalAlignment.Center;
+            wsGeneralLedger["A1:F1"].Value = "PCHS General Ledger";
+            wsGeneralLedger["A1:F1"].Style.Font.SetColor("#F0F0F0");
+            wsGeneralLedger["A1:F1"].Style.SetBackgroundColor("#125EA5");
+            //wsGeneralLedger["A1:F1"].Style.TopBorder.SetColor("#ff6600");
+            //wsGeneralLedger["A1:F1"].Style.TopBorder.Type = IronXL.Styles.BorderType.Double;
+            #endregion
+
+            #region Workbook Column
+            wsGeneralLedger["A2:F2"].Style.HorizontalAlignment = IronXL.Styles.HorizontalAlignment.Center;
+            wsGeneralLedger["A2:F2"].Style.ShrinkToFit = true;
+
+            wsGeneralLedger["A2"].Value = "Date";
+            wsGeneralLedger["A2"].Style.Font.SetColor("#F0F0F0");
+            wsGeneralLedger["A2"].Style.SetBackgroundColor("#125EA5");
+            wsGeneralLedger["A2"].Style.BottomBorder.SetColor("#ff6600");
+            wsGeneralLedger["A2"].Style.BottomBorder.Type = IronXL.Styles.BorderType.Double;
+            wsGeneralLedger["A2"].Style.TopBorder.SetColor("#ff6600");
+            wsGeneralLedger["A2"].Style.TopBorder.Type = IronXL.Styles.BorderType.Double;
+
+            wsGeneralLedger["B2"].Value = "Journal Entry #";
+            wsGeneralLedger["B2"].Style.Font.SetColor("#F0F0F0");
+            wsGeneralLedger["B2"].Style.SetBackgroundColor("#125EA5");
+            wsGeneralLedger["B2"].Style.BottomBorder.SetColor("#ff6600");
+            wsGeneralLedger["B2"].Style.BottomBorder.Type = IronXL.Styles.BorderType.Double;
+            wsGeneralLedger["B2"].Style.TopBorder.SetColor("#ff6600");
+            wsGeneralLedger["B2"].Style.TopBorder.Type = IronXL.Styles.BorderType.Double;
+
+            wsGeneralLedger["C2"].Value = "Description";
+            wsGeneralLedger["C2"].Style.Font.SetColor("#F0F0F0");
+            wsGeneralLedger["C2"].Style.SetBackgroundColor("#125EA5");
+            wsGeneralLedger["C2"].Style.BottomBorder.SetColor("#ff6600");
+            wsGeneralLedger["C2"].Style.BottomBorder.Type = IronXL.Styles.BorderType.Double;
+            wsGeneralLedger["C2"].Style.TopBorder.SetColor("#ff6600");
+            wsGeneralLedger["C2"].Style.TopBorder.Type = IronXL.Styles.BorderType.Double;
+
+            wsGeneralLedger["D2"].Value = "Debit";
+            wsGeneralLedger["D2"].Style.Font.SetColor("#F0F0F0");
+            wsGeneralLedger["D2"].Style.SetBackgroundColor("#125EA5");
+            wsGeneralLedger["D2"].Style.BottomBorder.SetColor("#ff6600");
+            wsGeneralLedger["D2"].Style.BottomBorder.Type = IronXL.Styles.BorderType.Double;
+            wsGeneralLedger["D2"].Style.TopBorder.SetColor("#ff6600");
+            wsGeneralLedger["D2"].Style.TopBorder.Type = IronXL.Styles.BorderType.Double;
+
+            wsGeneralLedger["E2"].Value = "Credit";
+            wsGeneralLedger["E2"].Style.Font.SetColor("#F0F0F0");
+            wsGeneralLedger["E2"].Style.SetBackgroundColor("#125EA5");
+            wsGeneralLedger["E2"].Style.BottomBorder.SetColor("#ff6600");
+            wsGeneralLedger["E2"].Style.BottomBorder.Type = IronXL.Styles.BorderType.Double;
+            wsGeneralLedger["E2"].Style.TopBorder.SetColor("#ff6600");
+            wsGeneralLedger["E2"].Style.TopBorder.Type = IronXL.Styles.BorderType.Double;
+
+            wsGeneralLedger["F2"].Value = "Balance";
+            wsGeneralLedger["F2"].Style.Font.SetColor("#F0F0F0");
+            wsGeneralLedger["F2"].Style.SetBackgroundColor("#125EA5");
+            wsGeneralLedger["F2"].Style.BottomBorder.SetColor("#ff6600");
+            wsGeneralLedger["F2"].Style.BottomBorder.Type = IronXL.Styles.BorderType.Double;
+            wsGeneralLedger["F2"].Style.TopBorder.SetColor("#ff6600");
+            wsGeneralLedger["F2"].Style.TopBorder.Type = IronXL.Styles.BorderType.Double;
+
+            #endregion
+
+            if (Directory.Exists(@"F:\Workbooks"))
+            {
+                wb.SaveAs($@"F:\Workbooks\{filename}.xlsx");
+                MessageBox.Show("File Created!");
+            }
+            else
+            {
+                DialogResult dr = MessageBox.Show(@"F:\Workbooks directory does not exists! \n\nDo you want me to create a directory?", "Directory Does Not Exist!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    Directory.CreateDirectory(@"F:\Workbooks");
+                    wb.SaveAs($@"F:\Workbooks\{filename}.xlsx");
+                    MessageBox.Show($@"Directory and {filename}.xlsx Created!");
+                }
+                else
+                {
+                    // do nothing
+                }
+            }
+            
+        }
+
+        public bool CheckFileExists(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ PCHS_FINANCE_SYSTEM/MainWindow.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace PCHS_FINANCE_SYSTEM
+{
+    partial class btnDisplayDrives
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.workBooktxtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(19, 69);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(223, 23);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create New Workbook";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // workBooktxtBox
+            // 
+            this.workBooktxtBox.Location = new System.Drawing.Point(19, 43);
+            this.workBooktxtBox.Name = "workBooktxtBox";
+            this.workBooktxtBox.Size = new System.Drawing.Size(223, 20);
+            this.workBooktxtBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Workbook Filename:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnCreate);
+            this.groupBox1.Controls.Add(this.workBooktxtBox);
+            this.groupBox1.Location = new System.Drawing.Point(17, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 108);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Create New Workbook";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(17, 17);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(242, 158);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Insert Data In Current Workbook";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(751, 466);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(743, 440);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Create";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tabPage2.Controls.Add(this.groupBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(743, 440);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Insert";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(278, 17);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(293, 108);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Add New Worksheet";
+            // 
+            // btnDisplayDrives
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(775, 490);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "btnDisplayDrives";
+            this.Text = "MainWindow";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox workBooktxtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
