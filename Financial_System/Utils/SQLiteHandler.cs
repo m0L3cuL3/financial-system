fb5f3fb5c46891cdf564768eeafb4563df5a415f
m0L3cuL3 <skidrowloler@gmail.com>
2021-02-16 20:38:27 +0800

    Added Utils folder and SQLiteHandler.

---
+++ Financial_System/Utils/SQLiteHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+
+namespace Financial_System.Utils
+{
+    interface ISQLite
+    {
+        SQLiteConnection CreateConnection();
+        void CreateTable(SQLiteConnection conn);
+    }
+
+    class SQLiteHandler : ISQLite
+    {
+        public SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection sqlite_conn;
+
+            // Create a new database connection:
+            sqlite_conn = new SQLiteConnection("Data Source=database.db; Version = 3; New = True; Compress = True; ");
+             
+            // Open the connection:
+             
+            try 
+            {
+                 sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return sqlite_conn;
+        }
+
+        public void CreateTable(SQLiteConnection conn)
+        {
+            SQLiteCommand sqlite_cmd;
+            string Createsql = "CREATE TABLE SampleTable(Col1 VARCHAR(20), Col2 INT)";
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = Createsql;
+            sqlite_cmd.ExecuteNonQuery();
+        }
+    }
+}
