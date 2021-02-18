4e028cf20619f81aad6ef91288b728901a5a8097
m0L3cuL3 <skidrowloler@gmail.com>
2021-02-18 21:33:10 +0800

    Added Insert and Read functions.

--- Financial_System/Utils/SQLiteHandler.cs
+++ Financial_System/Utils/SQLiteHandler.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
-
+using System.Windows.Forms;
 
 namespace Financial_System.Utils
 {
@@ -40,23 +40,62 @@ namespace Financial_System.Utils
         {
             SQLiteCommand sqlite_cmd;
 
-            // GENERAL LEDGER //
-            string GeneralLedger = "CREATE TABLE GeneralLedger(Date VARCHAR(20), Account VARCHAR(20), RefType VARCHAR(20), Debit INT, Credit INT, Balance INT)";
+            // STUDENT //
+            string GeneralLedger = "CREATE TABLE Student(sid INTEGER PRIMARY KEY AUTOINCREMENT, FirstName VARCHAR NOT NULL, MiddleName VARCHAR NOT NULL, Surname VARCHAR NOT NULL, Section VARCHAR NOT NULL, YearLevel INT NOT NULL);";
             sqlite_cmd = conn.CreateCommand();
             sqlite_cmd.CommandText = GeneralLedger;
             sqlite_cmd.ExecuteNonQuery();
 
-            // ACCOUNTS //
-            string Accounts = "CREATE TABLE Accounts(AccountName VARCHAR(20), RootType VARCHAR(20), AccountType VARCHAR(20))";
+            // STUDENT_TRANSACTION //
+            string Accounts = "CREATE TABLE Student_Transaction(tid INTEGER PRIMARY KEY AUTOINCREMENT, Term VARCHAR NOT NULL, Type VARCHAR NOT NULL, Amount INT NOT NULL, sid INT NOT NULL, FOREIGN KEY (sid) REFERENCES Student(sid));";
             sqlite_cmd = conn.CreateCommand();
             sqlite_cmd.CommandText = Accounts;
             sqlite_cmd.ExecuteNonQuery();
 
-            // JOURNAL ENTRIES //
-            string JournalEntries = "CREATE TABLE JournalEntries()"; // -> create table for journal entries.
+            // TRANSACTION_LEDGER //
+            string JournalEntries = "CREATE TABLE Transaction_Ledger(lid INTEGER PRIMARY KEY AUTOINCREMENT, Fullname VARCHAR NOT NULL, sid INT NOT NULL, FOREIGN KEY(sid) REFERENCES Student(sid));";
             sqlite_cmd = conn.CreateCommand();
             sqlite_cmd.CommandText = JournalEntries;
             sqlite_cmd.ExecuteNonQuery();
+        }
+
+        public void InsertStudentData(SQLiteConnection conn)
+        {
+            SQLiteCommand sqlite_cmd;
+
+            string insertData = "INSERT INTO Student(FirstName, MiddleName, Surname, Section, YearLevel) VALUES (@fname, @midname, @surname, @section, @yearLevel);";
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = insertData;
+
+            sqlite_cmd.Parameters.AddWithValue("@fname", "John");
+            sqlite_cmd.Parameters.AddWithValue("@midname", "Richard");
+            sqlite_cmd.Parameters.AddWithValue("@surname", "Doe");
+            sqlite_cmd.Parameters.AddWithValue("@section", "Section X");
+            sqlite_cmd.Parameters.AddWithValue("@yearLevel", 1);
+
+            sqlite_cmd.ExecuteNonQuery();
+        }
+
+        public void ReadStudentData(SQLiteConnection conn, DataGridView dgv)
+        {
+            SQLiteCommand sqlite_cmd;
+
+            sqlite_cmd = new SQLiteCommand("SELECT * FROM Student", conn);
+
+            using(SQLiteDataReader read = sqlite_cmd.ExecuteReader())
+            {
+                while (read.Read())
+                {
+                    dgv.Rows.Add(new object[] {
+                        read.GetValue(read.GetOrdinal("sid")),
+                        read.GetValue(read.GetOrdinal("FirstName")),  // Or column name like this
+                        read.GetValue(read.GetOrdinal("MiddleName")),
+                        read.GetValue(read.GetOrdinal("Surname")),
+                        read.GetValue(read.GetOrdinal("Section")),
+                        read.GetValue(read.GetOrdinal("YearLevel"))
+                    });
+                }
+            }
         }
     }
 }
