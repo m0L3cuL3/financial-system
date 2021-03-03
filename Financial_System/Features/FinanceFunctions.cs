0b049afda6ea962c45227c58b15cee3072b9046a
m0L3cuL3 <skidrowloler@gmail.com>
2021-03-03 17:08:11 +0800

    Added FinancialFunction.cs

---
+++ Financial_System/Features/FinanceFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using srbrettle.FinancialFormulas;
+
+namespace Financial_System.Features
+{
+    class FinanceFunctions
+    {
+
+        public decimal CalcAssets(decimal liabilities, decimal equity)
+        {
+            decimal result = FinancialFormulas.CalcAssets(liabilities, equity);
+            return result;
+        }
+
+        public decimal CalcEquity(decimal assets, decimal liabilities)
+        {
+            decimal result = FinancialFormulas.CalcEquity(assets, liabilities);
+            return result;
+        }
+
+    }
+}
