421bf904c69d7710d926b34ff9009c88908d1312
Michael Pagaran <mmpagaran64@gmail.com>
2021-03-13 09:46:48 +0800

    initial commit

--- Financial_System/UserControls/ReportsControl.Designer.cs
+++ Financial_System/UserControls/ReportsControl.Designer.cs
@@ -34,13 +34,15 @@
             this.StudentIdColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.TypeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.AmountColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // listView1
             // 
+            this.listView1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(19, 21);
+            this.listView1.Location = new System.Drawing.Point(14, 76);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(296, 188);
             this.listView1.TabIndex = 0;
@@ -49,13 +51,14 @@
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.TransIdColumn,
             this.StudentIdColumn,
             this.TypeColumn,
             this.AmountColumn});
-            this.dataGridView1.Location = new System.Drawing.Point(330, 21);
+            this.dataGridView1.Location = new System.Drawing.Point(328, 76);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(455, 188);
@@ -93,10 +96,22 @@
             this.AmountColumn.ReadOnly = true;
             this.AmountColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ebrima", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(14)))), ((int)(((byte)(42)))));
+            this.label1.Location = new System.Drawing.Point(38, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // ReportsControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.listView1);
             this.Name = "ReportsControl";
@@ -104,6 +119,7 @@
             this.Load += new System.EventHandler(this.ReportsControl_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -115,5 +131,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn StudentIdColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn TypeColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn AmountColumn;
+        private System.Windows.Forms.Label label1;
     }
 }
