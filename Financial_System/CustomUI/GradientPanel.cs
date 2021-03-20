160403e55e36f84a5a937ee91601c3cbf3268342
m0L3cuL3 <skidrowloler@gmail.com>
2021-03-20 17:50:54 +0800

    Added Custom Panel

---
+++ Financial_System/CustomUI/GradientPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Financial_System.CustomUI
+{
+    public class GradientPanel : Panel
+    {
+        public Color TopColor { get; set; }
+
+        public Color BottomColor { get; set; }
+
+        public float Angle { get; set; }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, this.TopColor, this.BottomColor, this.Angle);
+            Graphics g = e.Graphics;
+            g.FillRectangle(brush, this.ClientRectangle);
+            base.OnPaint(e);
+        }
+    }
+}
