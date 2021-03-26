ad073b8a7c4e412f33146d03b4088c9fb210eeef
m0L3cuL3 <skidrowloler@gmail.com>
2021-03-26 19:48:44 +0800

    Filter and Database updates. :smiley:

--- Financial_System/Program.cs
+++ Financial_System/Program.cs
@@ -1,8 +1,5 @@
 ﻿using Financial_System.Forms;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Financial_System
