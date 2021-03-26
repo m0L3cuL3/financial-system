d102a531de056612dab9ac2d8fbaa2c1ae158517
m0L3cuL3 <skidrowloler@gmail.com>
2021-03-26 19:48:26 +0800

    Added default lists. :smiley:

---
+++ Financial_System/Globals.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financial_System
+{
+    public class Globals
+    {
+        public string[] PaymentList =
+        {
+            "Tuition Payment",
+            "Book Payment",
+            "Miscellanous Payment",
+            "Laboratory Payment"
+        };
+
+        public string[] FeeList =
+        {
+            "Tuition Fee",
+            "Book Fee",
+            "Miscellanous Fee",
+            "Laboratory Fee"
+        };
+
+        public string[] MonthList =
+        {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        };
+
+        public string[] MonthIndex =
+        {
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+        };
+    }
+}
