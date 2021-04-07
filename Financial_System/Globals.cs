2979b25cfa8a414083b8fd8befeb13afa49606ef
m0L3cuL3 <skidrowloler@gmail.com>
2021-04-08 00:57:38 +0800

    Updates! :shipit:

--- Financial_System/Globals.cs
+++ Financial_System/Globals.cs
@@ -24,6 +24,22 @@ namespace Financial_System
             "Laboratory Fee"
         };
 
+        public string[] LevelList =
+        {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+        };
+
         public string[] MonthList =
         {
             "January",
