cb17855996dbbb490abdda67a7b4b068d9f62983
m0L3cuL3 <skidrowloler@gmail.com>
2021-04-10 15:00:11 +0800

    Updated and fixed some bugs in LiveCharts.

--- Financial_System/UserControls/DashboardControl.Designer.cs
+++ Financial_System/UserControls/DashboardControl.Designer.cs
@@ -29,16 +29,21 @@ namespace Financial_System.UserControls
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.TitleLabel = new System.Windows.Forms.Label();
             this.CollectablesPieChart = new LiveCharts.WinForms.PieChart();
             this.CollectablesPanel = new System.Windows.Forms.Panel();
             this.FeeCollectionsLabel = new System.Windows.Forms.Label();
-            this.NetConStatusLabel = new System.Windows.Forms.Label();
             this.NetConLabel = new System.Windows.Forms.Label();
             this.InfoLabel = new System.Windows.Forms.Label();
             this.InfoPanel = new System.Windows.Forms.Panel();
-            this.DBFileNameLabel = new System.Windows.Forms.Label();
-            this.DBFileNameTitleLabel = new System.Windows.Forms.Label();
+            this.YearTextBox = new System.Windows.Forms.TextBox();
+            this.ShowReportsButton = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.NetLatencyLabel = new System.Windows.Forms.Label();
+            this.network_timer = new System.Windows.Forms.Timer(this.components);
+            this.conStatusLabel = new System.Windows.Forms.Label();
+            this.pingStatusLabel = new System.Windows.Forms.Label();
             this.CollectablesPanel.SuspendLayout();
             this.InfoPanel.SuspendLayout();
             this.SuspendLayout();
@@ -60,16 +65,19 @@ namespace Financial_System.UserControls
             this.CollectablesPieChart.ForeColor = System.Drawing.Color.Black;
             this.CollectablesPieChart.Location = new System.Drawing.Point(12, 14);
             this.CollectablesPieChart.Name = "CollectablesPieChart";
-            this.CollectablesPieChart.Size = new System.Drawing.Size(279, 227);
+            this.CollectablesPieChart.Size = new System.Drawing.Size(258, 227);
             this.CollectablesPieChart.TabIndex = 0;
             // 
             // CollectablesPanel
             // 
             this.CollectablesPanel.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.CollectablesPanel.Controls.Add(this.label15);
+            this.CollectablesPanel.Controls.Add(this.ShowReportsButton);
+            this.CollectablesPanel.Controls.Add(this.YearTextBox);
             this.CollectablesPanel.Controls.Add(this.CollectablesPieChart);
             this.CollectablesPanel.Location = new System.Drawing.Point(23, 90);
             this.CollectablesPanel.Name = "CollectablesPanel";
-            this.CollectablesPanel.Size = new System.Drawing.Size(306, 262);
+            this.CollectablesPanel.Size = new System.Drawing.Size(384, 255);
             this.CollectablesPanel.TabIndex = 2;
             // 
             // FeeCollectionsLabel
@@ -78,26 +86,15 @@ namespace Financial_System.UserControls
             this.FeeCollectionsLabel.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FeeCollectionsLabel.Location = new System.Drawing.Point(19, 64);
             this.FeeCollectionsLabel.Name = "FeeCollectionsLabel";
-            this.FeeCollectionsLabel.Size = new System.Drawing.Size(221, 20);
+            this.FeeCollectionsLabel.Size = new System.Drawing.Size(194, 20);
             this.FeeCollectionsLabel.TabIndex = 3;
-            this.FeeCollectionsLabel.Text = "Collection Report (by Month)";
-            // 
-            // NetConStatusLabel
-            // 
-            this.NetConStatusLabel.AutoSize = true;
-            this.NetConStatusLabel.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NetConStatusLabel.Location = new System.Drawing.Point(224, 31);
-            this.NetConStatusLabel.Name = "NetConStatusLabel";
-            this.NetConStatusLabel.Size = new System.Drawing.Size(67, 17);
-            this.NetConStatusLabel.TabIndex = 7;
-            this.NetConStatusLabel.Text = "netStatus";
-            this.NetConStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.FeeCollectionsLabel.Text = "Collection Report (Yearly)";
             // 
             // NetConLabel
             // 
             this.NetConLabel.AutoSize = true;
             this.NetConLabel.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NetConLabel.Location = new System.Drawing.Point(15, 31);
+            this.NetConLabel.Location = new System.Drawing.Point(9, 16);
             this.NetConLabel.Name = "NetConLabel";
             this.NetConLabel.Size = new System.Drawing.Size(148, 17);
             this.NetConLabel.TabIndex = 6;
@@ -109,42 +106,93 @@ namespace Financial_System.UserControls
             this.InfoLabel.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.InfoLabel.Location = new System.Drawing.Point(19, 368);
             this.InfoLabel.Name = "InfoLabel";
-            this.InfoLabel.Size = new System.Drawing.Size(157, 20);
+            this.InfoLabel.Size = new System.Drawing.Size(160, 20);
             this.InfoLabel.TabIndex = 7;
-            this.InfoLabel.Text = "General Information";
+            this.InfoLabel.Text = "Network Information";
             // 
             // InfoPanel
             // 
             this.InfoPanel.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.InfoPanel.Controls.Add(this.NetConStatusLabel);
-            this.InfoPanel.Controls.Add(this.DBFileNameLabel);
+            this.InfoPanel.Controls.Add(this.pingStatusLabel);
+            this.InfoPanel.Controls.Add(this.conStatusLabel);
+            this.InfoPanel.Controls.Add(this.NetLatencyLabel);
             this.InfoPanel.Controls.Add(this.NetConLabel);
-            this.InfoPanel.Controls.Add(this.DBFileNameTitleLabel);
             this.InfoPanel.Location = new System.Drawing.Point(23, 391);
             this.InfoPanel.Name = "InfoPanel";
-            this.InfoPanel.Size = new System.Drawing.Size(306, 65);
+            this.InfoPanel.Size = new System.Drawing.Size(248, 65);
             this.InfoPanel.TabIndex = 6;
             // 
-            // DBFileNameLabel
+            // YearTextBox
             // 
-            this.DBFileNameLabel.AutoSize = true;
-            this.DBFileNameLabel.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DBFileNameLabel.Location = new System.Drawing.Point(205, 14);
-            this.DBFileNameLabel.Name = "DBFileNameLabel";
-            this.DBFileNameLabel.Size = new System.Drawing.Size(86, 17);
-            this.DBFileNameLabel.TabIndex = 7;
-            this.DBFileNameLabel.Text = "dbFileName";
-            this.DBFileNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.YearTextBox.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.YearTextBox.Location = new System.Drawing.Point(276, 32);
+            this.YearTextBox.Name = "YearTextBox";
+            this.YearTextBox.Size = new System.Drawing.Size(97, 21);
+            this.YearTextBox.TabIndex = 17;
             // 
-            // DBFileNameTitleLabel
+            // ShowReportsButton
             // 
-            this.DBFileNameTitleLabel.AutoSize = true;
-            this.DBFileNameTitleLabel.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DBFileNameTitleLabel.Location = new System.Drawing.Point(15, 14);
-            this.DBFileNameTitleLabel.Name = "DBFileNameTitleLabel";
-            this.DBFileNameTitleLabel.Size = new System.Drawing.Size(148, 17);
-            this.DBFileNameTitleLabel.TabIndex = 6;
-            this.DBFileNameTitleLabel.Text = "Database File Name: ";
+            this.ShowReportsButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(58)))), ((int)(((byte)(108)))));
+            this.ShowReportsButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ShowReportsButton.FlatAppearance.BorderSize = 0;
+            this.ShowReportsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ShowReportsButton.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.ShowReportsButton.ForeColor = System.Drawing.Color.White;
+            this.ShowReportsButton.Location = new System.Drawing.Point(276, 215);
+            this.ShowReportsButton.Name = "ShowReportsButton";
+            this.ShowReportsButton.Size = new System.Drawing.Size(97, 26);
+            this.ShowReportsButton.TabIndex = 18;
+            this.ShowReportsButton.Text = "Show Reports";
+            this.ShowReportsButton.UseVisualStyleBackColor = false;
+            this.ShowReportsButton.Click += new System.EventHandler(this.ShowReportsButton_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.Color.DimGray;
+            this.label15.Location = new System.Drawing.Point(276, 14);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(33, 15);
+            this.label15.TabIndex = 44;
+            this.label15.Text = "Year";
+            // 
+            // NetLatencyLabel
+            // 
+            this.NetLatencyLabel.AutoSize = true;
+            this.NetLatencyLabel.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NetLatencyLabel.Location = new System.Drawing.Point(9, 36);
+            this.NetLatencyLabel.Name = "NetLatencyLabel";
+            this.NetLatencyLabel.Size = new System.Drawing.Size(121, 17);
+            this.NetLatencyLabel.TabIndex = 8;
+            this.NetLatencyLabel.Text = "Network Latency:";
+            // 
+            // network_timer
+            // 
+            this.network_timer.Interval = 1000;
+            this.network_timer.Tick += new System.EventHandler(this.network_timer_Tick);
+            // 
+            // conStatusLabel
+            // 
+            this.conStatusLabel.AutoSize = true;
+            this.conStatusLabel.BackColor = System.Drawing.Color.Transparent;
+            this.conStatusLabel.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conStatusLabel.Location = new System.Drawing.Point(176, 16);
+            this.conStatusLabel.Name = "conStatusLabel";
+            this.conStatusLabel.Size = new System.Drawing.Size(33, 17);
+            this.conStatusLabel.TabIndex = 9;
+            this.conStatusLabel.Text = "N/A";
+            // 
+            // pingStatusLabel
+            // 
+            this.pingStatusLabel.AutoSize = true;
+            this.pingStatusLabel.BackColor = System.Drawing.Color.Transparent;
+            this.pingStatusLabel.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pingStatusLabel.Location = new System.Drawing.Point(176, 36);
+            this.pingStatusLabel.Name = "pingStatusLabel";
+            this.pingStatusLabel.Size = new System.Drawing.Size(33, 17);
+            this.pingStatusLabel.TabIndex = 10;
+            this.pingStatusLabel.Text = "N/A";
             // 
             // DashboardControl
             // 
@@ -159,6 +207,7 @@ namespace Financial_System.UserControls
             this.Name = "DashboardControl";
             this.Size = new System.Drawing.Size(848, 478);
             this.CollectablesPanel.ResumeLayout(false);
+            this.CollectablesPanel.PerformLayout();
             this.InfoPanel.ResumeLayout(false);
             this.InfoPanel.PerformLayout();
             this.ResumeLayout(false);
@@ -172,11 +221,15 @@ namespace Financial_System.UserControls
         private LiveCharts.WinForms.PieChart CollectablesPieChart;
         private System.Windows.Forms.Panel CollectablesPanel;
         private System.Windows.Forms.Label FeeCollectionsLabel;
-        private System.Windows.Forms.Label NetConStatusLabel;
         private System.Windows.Forms.Label NetConLabel;
         private System.Windows.Forms.Label InfoLabel;
         private System.Windows.Forms.Panel InfoPanel;
-        private System.Windows.Forms.Label DBFileNameLabel;
-        private System.Windows.Forms.Label DBFileNameTitleLabel;
+        private System.Windows.Forms.TextBox YearTextBox;
+        private System.Windows.Forms.Button ShowReportsButton;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label NetLatencyLabel;
+        private System.Windows.Forms.Timer network_timer;
+        private System.Windows.Forms.Label pingStatusLabel;
+        private System.Windows.Forms.Label conStatusLabel;
     }
 }
--- Financial_System/UserControls/DashboardControl.cs
+++ Financial_System/UserControls/DashboardControl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Windows.Forms;
 using Financial_System.Utils;
 using LiveCharts;
@@ -15,37 +16,41 @@ namespace Financial_System.UserControls
         SQLiteHandler sql = new SQLiteHandler();
         UIHandler ui = new UIHandler();
         Globals gb = new Globals();
+
+        string currYear = DateTime.Now.Year.ToString();
+        string currMonth = DateTime.Now.Month.ToString();
+
         public DashboardControl()
         {
             InitializeComponent();
             ui.RoundPanel(CollectablesPanel);
             ui.RoundPanel(InfoPanel);
             CheckNetworkConnection();
-            CheckDatabaseStatus();
-            LoadChartData();
         }
+
 
         Func<ChartPoint, string> labelPoint = chartpoint => string.Format("{0} ({1:P})", chartpoint.Y, chartpoint.Participation);
 
-        // make this load actual data, next time.
-        public void LoadChartData()
+        // Load Current Year Collection Reports
+        public void LoadCurrReports()
         {
+            
             try
             {
                 SeriesCollection series = new SeriesCollection();
 
-                series.Add(new PieSeries() { Title = gb.MonthList[0], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[0]) }, DataLabels = true, LabelPoint = labelPoint });
-                series.Add(new PieSeries() { Title = gb.MonthList[1], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[1]) }, DataLabels = true, LabelPoint = labelPoint });
-                series.Add(new PieSeries() { Title = gb.MonthList[2], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[2]) }, DataLabels = true, LabelPoint = labelPoint });
-                series.Add(new PieSeries() { Title = gb.MonthList[3], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[3]) }, DataLabels = true, LabelPoint = labelPoint });
-                series.Add(new PieSeries() { Title = gb.MonthList[4], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[4]) }, DataLabels = true, LabelPoint = labelPoint });
-                series.Add(new PieSeries() { Title = gb.MonthList[5], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[5]) }, DataLabels = true, LabelPoint = labelPoint });
-                series.Add(new PieSeries() { Title = gb.MonthList[6], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[6]) }, DataLabels = true, LabelPoint = labelPoint });
-                series.Add(new PieSeries() { Title = gb.MonthList[7], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[7]) }, DataLabels = true, LabelPoint = labelPoint });
-                series.Add(new PieSeries() { Title = gb.MonthList[8], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[8]) }, DataLabels = true, LabelPoint = labelPoint });
-                series.Add(new PieSeries() { Title = gb.MonthList[9], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[9]) }, DataLabels = true, LabelPoint = labelPoint });
-                series.Add(new PieSeries() { Title = gb.MonthList[10], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[10]) }, DataLabels = true, LabelPoint = labelPoint });
-                series.Add(new PieSeries() { Title = gb.MonthList[11], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[11]) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[0], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[0], currYear) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[1], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[1], currYear) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[2], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[2], currYear) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[3], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[3], currYear) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[4], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[4], currYear) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[5], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[5], currYear) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[6], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[6], currYear) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[7], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[7], currYear) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[8], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[8], currYear) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[9], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[9], currYear) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[10], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[10], currYear) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[11], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[11], currYear) }, DataLabels = true, LabelPoint = labelPoint });
 
                 CollectablesPieChart.Series = series;
 
@@ -61,6 +66,40 @@ namespace Financial_System.UserControls
             }   
         }
 
+        // Load Specific Year Collection Reports
+        private void LoadReports()
+        {
+            try
+            {
+                SeriesCollection series = new SeriesCollection();
+
+                series.Add(new PieSeries() { Title = gb.MonthList[0], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[0], YearTextBox.Text) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[1], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[1], YearTextBox.Text) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[2], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[2], YearTextBox.Text) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[3], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[3], YearTextBox.Text) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[4], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[4], YearTextBox.Text) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[5], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[5], YearTextBox.Text) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[6], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[6], YearTextBox.Text) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[7], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[7], YearTextBox.Text) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[8], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[8], YearTextBox.Text) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[9], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[9], YearTextBox.Text) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[10], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[10], YearTextBox.Text) }, DataLabels = true, LabelPoint = labelPoint });
+                series.Add(new PieSeries() { Title = gb.MonthList[11], Values = new ChartValues<int> { sql.GetTotalTransByMonth(sql.CreateConnection(), gb.MonthIndex[11], YearTextBox.Text) }, DataLabels = true, LabelPoint = labelPoint });
+
+                CollectablesPieChart.Series = series;
+
+                CollectablesPieChart.LegendLocation = LegendLocation.Bottom;
+
+                // fix for Negative/Black PieChart (Sean Baang)
+                CollectablesPieChart.Hide();
+                CollectablesPieChart.Show();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
         // checks network connection.
         private void CheckNetworkConnection()
         {
@@ -68,27 +107,28 @@ namespace Financial_System.UserControls
             {
                 using (var client = new WebClient())
                 using (client.OpenRead("http://google.com"))
-                NetConStatusLabel.ForeColor = Color.DodgerBlue;
-                NetConStatusLabel.Text = "online";
+                conStatusLabel.ForeColor = Color.DodgerBlue;
+                conStatusLabel.Text = "online";
+                network_timer.Start();
             }
             catch
             {
-                NetConStatusLabel.ForeColor = Color.Red;
-                NetConStatusLabel.Text = "offline";
+                conStatusLabel.ForeColor = Color.Red;
+                conStatusLabel.Text = "offline";
             }
         }
 
-        // checks database status. (as of now, it displays only the file)
-        private void CheckDatabaseStatus()
+        private void ShowReportsButton_Click(object sender, EventArgs e)
         {
-            string dbName = "database.db";
-            DirectoryInfo path = new DirectoryInfo(@"\");
-            string filename = null;
+            LoadReports();
+        }
 
-            filename = Path.GetFileName(path + dbName);
-
-            DBFileNameLabel.ForeColor = Color.DodgerBlue;
-            DBFileNameLabel.Text = filename;
+        private void network_timer_Tick(object sender, EventArgs e)
+        {
+            Ping pc = new Ping();
+            PingReply pr = pc.Send("www.google.com");
+            pingStatusLabel.ForeColor = Color.DodgerBlue;
+            pingStatusLabel.Text = $"{pr.RoundtripTime} ms";
         }
     }
 }
